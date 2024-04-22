c12481c1ad863a6d26d7e1c0a5f52da5acb1a21c
JoseGlez <A01028517@tec.mx>
2024-04-22 14:37:38 -0600

    Api example

---
+++ DesWeb/Ejemplos_api/hello_server/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import express from 'express'
+
+const app = express()
+
+let card_list =[]
+
+app.use(express.json())
+
+app.get("/name", (req, res)=>{
+    const salute="hellow from server"
+    res.status(200).send(salute)
+})
+
+app.get("/hello/:name", (req,res)=>{
+    console.log(req.params)
+    const salute=`hello ${req.params.name}!!`
+    res.status(200).send(salute)
+})
+
+app.post('/cards', (req,res)=>{
+    console.log(req.body)
+    card_list.push(req.body)
+    res.status(200).send("Card added succesfully")
+})
+
+app.listen(5000, ()=>{
+    console.log("running on port 5000")
+})
+
