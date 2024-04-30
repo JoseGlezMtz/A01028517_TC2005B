035f3f4428aa38f53b3f937e8a1c0ceb58321018
JoseGlez <A01028517@tec.mx>
2024-04-30 13:46:03 -0600

    avance simon

---
+++ Videogames/Clase 1/Assets/Scripts/Simon/SimonButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimonButton : MonoBehaviour
+{
+    Color originalColor;
+    public float delay;
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    public void init(int index)
+    {
+      originalColor=GetComponent<Image>().color; 
+      audio=GetComponent<AudioSource>();
+      audio.clip=Resources.Load<AudioClip>($"Audios/{index}");
+    }
+
+    // Update is called once per frame
+    public void Highlight()
+    {
+        audio.Play();
+        StartCoroutine(ChangeColor());
+    }
+
+    IEnumerator ChangeColor()
+    {
+        GetComponent<Image>().color=Color.white;
+        yield return new WaitForSeconds(delay);
+        GetComponent<Image>().color=originalColor;
+    }
+}
