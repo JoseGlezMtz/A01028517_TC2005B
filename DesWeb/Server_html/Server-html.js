104e1b7d648055fb72c35e8c2030acc1e4b7c520
JoseGlez <A01028517@tec.mx>
2024-05-21 18:33:09 -0600

    css cheat sheet

--- DesWeb/Server_html/Server-html.js
+++ DesWeb/Server_html/Server-html.js
@@ -6,9 +6,13 @@ import fs from "fs"
 const app = express()
 app.use(express.static("public"))
 
-app.get("/", (req,res)=>{
+app.get("/html_cheat_sheet", (req,res)=>{
     const file=fs.readFileSync("public/html/html_cheat_sheet.html", "utf8")
     res.status(200).send(file)})
+
+    app.get("/Css_cheat_sheet", (req,res)=>{
+        const file=fs.readFileSync("public/html/Css_cheat_sheet.html", "utf8")
+        res.status(200).send(file)})
 
 app.listen(7000, ()=>{
     console.log("running on port 7000")
